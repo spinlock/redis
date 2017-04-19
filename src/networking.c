6bdffb14f3a21ebe5b6219079c4fd6357b40e2e3
spinlock <wnzheng@gmail.com>
2017-04-19 12:42:07 +0800

    MigrateAsync: declare macros & glue code

--- src/networking.c
+++ src/networking.c
@@ -145,6 +145,7 @@ client *createClient(int fd) {
     c->pubsub_patterns = listCreate();
     c->peerid = NULL;
     c->client_list_node = NULL;
+    c->async_migration = NULL;
     listSetFreeMethod(c->pubsub_patterns,decrRefCountVoid);
     listSetMatchMethod(c->pubsub_patterns,listMatchObjects);
     if (fd != -1) linkClient(c);
@@ -829,6 +830,10 @@ void freeClient(client *c) {
     if ((c->flags & CLIENT_SLAVE) && !(c->flags & CLIENT_MONITOR)) {
         serverLog(LL_WARNING,"Connection with slave %s lost.",
             replicationGetSlaveName(c));
+    }
+
+    if (c->flags & CLIENT_ASYNC_MIGRATION) {
+        releaseClientFromAsyncMigration(c);
     }
 
     /* Free the query buffer */
@@ -1916,6 +1921,11 @@ char *getClientTypeName(int class) {
 int checkClientOutputBufferLimits(client *c) {
     int soft = 0, hard = 0, class;
     unsigned long used_mem = getClientOutputBufferMemoryUsage(c);
+
+    /* Just ignore the output buffer limits if client is used asynchronous migration. */
+    if (c->flags & CLIENT_ASYNC_MIGRATION) {
+        return 0;
+    }
 
     class = getClientType(c);
     /* For the purpose of output buffer limiting, masters are handled
